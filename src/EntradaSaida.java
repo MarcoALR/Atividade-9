aed7e89d30705c3df6c370f4787e2c229f8533d9
Itallo <itallorocha12@gmail.com>
2025-05-07 18:35:23 -0300

    🔥

--- src/EntradaSaida.java
+++ src/EntradaSaida.java
@@ -1,16 +1,20 @@
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
 public class EntradaSaida {
 
-    public static void exibirMensagem(String mensagem) {
-        System.out.println(mensagem);
-
+    public static String lerString(String mensagem) {
+        return JOptionPane.showInputDialog(mensagem);
     }
 
+    public static int lerInteiro(String mensagem) {
+        try {
+            return Integer.parseInt(lerString(mensagem));
+        } catch (Exception e) {
+            return 0;
+        }
+    }
 
-
-
-
-
+    public static void exibirMensagem(String mensagem) {
+        JOptionPane.showMessageDialog(null, mensagem);
+    }
 }
-
